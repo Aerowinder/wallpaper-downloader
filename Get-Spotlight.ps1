06790cf9049d6282a4f593e35a42ef6cb611b20c
Aerowinder <51070353+Aerowinder@users.noreply.github.com>
2024-03-01 09:55:51 -0500

    v3, automatic windows10spotlight.com scraping

--- Get-Spotlight.ps1
+++ Get-Spotlight.ps1
@@ -1,29 +1,37 @@
 param([Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)][string]$destination)
 
-if (Test-Path $destination){
-    $dst_path_1080 = "$destination\1080x1920\"
-    $dst_path_1920 = "$destination\1920x1080\"
-    $dst_path_3840 = "$destination\3840x2160\"
-} else {
+if (!(Test-Path $destination)){
     Write-Host "ERROR: Provided destination folder does not exist." -ForegroundColor Red
     exit
 }
 
+#Local
 $src_path = "$($env:LOCALAPPDATA)\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets"
-
 foreach ($i in Get-ChildItem -Path $src_path) {
     $image = New-Object System.Drawing.Bitmap $i.Fullname
 
-    switch ($image.Width) {
-        #We are testing to see if the file exists in the destination path before copying. This is just to reduce OneDrive new file spam and to keep file dates accurate.
-        '1080' {if (-Not(Test-Path -Path ($dst_path_1080 + $i.Name + '.jpg'))) {Copy-Item -Path $i.FullName -Destination ($dst_path_1080 + $i.Name + '.jpg')}}
-        '1920' {if (-Not(Test-Path -Path ($dst_path_1920 + $i.Name + '.jpg'))) {Copy-Item -Path $i.FullName -Destination ($dst_path_1920 + $i.Name + '.jpg')}}
-        '3840' {if (-Not(Test-Path -Path ($dst_path_3840 + $i.Name + '.jpg'))) {Copy-Item -Path $i.FullName -Destination ($dst_path_3840 + $i.Name + '.jpg')}}
+    if ($image.Width -eq '1920' -And !(Test-Path -Path ($destination + '\' + $i.Name + '.jpg'))) {
+        Copy-Item -Path $i.FullName -Destination ($destination + '\' + $i.Name + '.jpg')
     }
 
     $image.Dispose()
 }
 
+#Remote
+$w10spotlight = Invoke-WebRequest https://windows10spotlight.com
+foreach ($image in $w10spotlight.Images.src) {
+    if ($image -notlike '*.jpg') {continue}
+
+    $web_path = $image.Replace('-1024x576','') #Remove thumbnail size info from name
+    $local_filename = $web_path -Replace '.*\/' #Pull out the filename of the .jpg from the URL
+
+    if (!(Test-Path -Path "$destination\$local_filename")) {
+        Invoke-WebRequest -Uri $web_path -OutFile "$destination\$local_filename"
+    }
+}
+
 #Changelog
 #2024-02-09 - AS - v1, First release.
-#2024-02-16 - As - v2, Commandline destination required for compatibility in multiple locations.
+#2024-02-16 - AS - v2, Commandline destination required for compatibility in multiple locations.
+#2024-03-01 - AS - v3, Added windows10spotlight.com scraping. Got tired of manually downloading.
+#                      Removed 1080 and 4K downloads. I don't use them and it would take more work to scrape the images from windows10spotlight.com.
